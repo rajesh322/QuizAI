56c31920b9a2efa8c9c94bb45f10678c7459c459
Tejas Vinay <tejasvinu@gmail.com>
2024-01-14 17:32:02 +0530

    Refactor Login component and add Login.css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,37 @@
 // src/components/Login.js
 
-
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Cookies from "js-cookie";
+import "../css/Login.css";
 
 const Login = () => {
-    const handleGoogleLogin = () => {
-        window.location.href = 'https://coral-app-rgl66.ondigitalocean.app/auth/google';
-    };
-    useEffect(() => {
-        try {
-            // Check if a JWT token exists in cookies
-            let token = Cookies.get('authToken');
-            console.log('Token:', token);
-            if (token) {
-                // If there's no token, redirect the user to the login page
-                window.location.href = 'https://testmindsai.tech/create';
-            }
-        } catch (error) {
-            console.error('Error verifying JWT token:', error);
-        }
-    }, []);
-    return (
-        <div className="login-container">
-            <h1 className="login-heading">Login</h1>
-            <button className="login-button" onClick={handleGoogleLogin}>
-                Login with Google
-            </button>
-        </div>
-    );
+  const handleGoogleLogin = () => {
+    window.location.href = 'https://coral-app-rgl66.ondigitalocean.app/auth/google';
+  };
+
+  useEffect(() => {
+    try {
+      // Check if a JWT token exists in cookies
+      let token = Cookies.get('authToken');
+      console.log('Token:', token);
+
+      if (token) {
+        // If there's no token, redirect the user to the login page
+        window.location.href = 'https://testmindsai.tech/create';
+      }
+    } catch (error) {
+      console.error('Error verifying JWT token:', error);
+    }
+  }, []);
+
+  return (
+    <div className="login-container">
+      <h1 className="login-heading">Login</h1>
+      <button className="login-button" onClick={handleGoogleLogin}>
+        Login with Google
+      </button>
+    </div>
+  );
 };
 
 export default Login;
