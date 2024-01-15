b3635b7ef87654b997bf95a8d60d21b5e019acde
Tejas Vinay <tejasvinu@gmail.com>
2024-01-15 14:24:29 +0530

    Refactor Login component imports and useHistory hook to useNavigate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,16 @@
 import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate  } from "react-router-dom";
 import "../css/Login.css";
-import { Agent as HttpAgent } from "http";
-import { Agent as HttpsAgent } from "https";
 
 const Login = () => {
-    const history = useHistory();
+    const history = useNavigate();
 
     const handleGoogleLogin = async () => {
-        const httpAgent = new HttpAgent({ keepAlive: true });
-        const httpsAgent = new HttpsAgent({ keepAlive: true });
 
         try {
             const response = await fetch('https://coral-app-rgl66.ondigitalocean.app/auth/google', {
                 method: 'GET',
-                credentials: 'include',
-                agent: (parsedURL) => parsedURL.protocol === 'http:' ? httpAgent : httpsAgent,
+                credentials: 'include'
             });
 
             if (!response.ok) {
