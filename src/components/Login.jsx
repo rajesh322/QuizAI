64bbb3c0aae1bc1a686a2d02ba5555c1832992ea
Tejas Vinay <tejasvinu@gmail.com>
2024-01-15 15:32:08 +0530

    Remove unused HTTP and HTTPS agents in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,16 @@
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
-import { Agent as HttpAgent } from "http";
-import { Agent as HttpsAgent } from "https";
 import { useNavigate } from "react-router-dom";
 import "../css/Login.css";
 
 const Login = () => {
     const history = useNavigate();
 
-    // Create HTTP and HTTPS agents with connection pooling
-    const httpAgent = new HttpAgent({ keepAlive: true });
-    const httpsAgent = new HttpsAgent({ keepAlive: true });
-
     const handleGoogleLogin = () => {
         axios.get('https://coral-app-rgl66.ondigitalocean.app/auth/google', {
             withCredentials: true,
-            credentials: 'include',
-            httpAgent, // Include the HTTP agent for connection pooling
-            httpsAgent, // Include the HTTPS agent for connection pooling
+            credentials: 'include'
         })
             .then(response => {
                 // Handle the response as needed
