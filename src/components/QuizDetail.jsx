47633c1419bdbbc1496b32b02ee8e01ba144eece
Tejas Vinay <tejasvinu@gmail.com>
2024-01-28 23:49:04 +0530

    Add markdown support for quiz questions and options

--- src/components/QuizDetail.jsx
+++ src/components/QuizDetail.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { API_URL } from '../constants';
+import Markdown from 'markdown-to-jsx';
 
 const QuizDetail = () => {
     const id = useParams().id;
@@ -65,70 +66,74 @@ const QuizDetail = () => {
         }
     };
     return (
-        <div className="container mt-4">
-            {submitted && result && (
-                <div className="text-center mb-4">
-                            <div className="quiz-result">
-                    <h4 className="text-center mb-4">Your Quiz Result</h4>
-                    <p className="lead">
-                        <h1 className="display-6">Score: {result.correctAnswers} / {result.totalQuestions}</h1>
-                    </p>
-                    <p>{getResultMessage()}</p>
-                </div>
-                </div>
-            )}
-
-            <form>
-                {quiz.questions.map((question, index) => (
-                    <div key={index} className={`mb-4 border p-3 ${submitted ? 'options' : ''}`}>
-                        <p className="lead">{`Question ${index + 1}: ${question.question}`}</p>
-                        <div className="row row-cols-2">
-                            {question.options.map((option, optionIndex) => (
-                                <div key={optionIndex} className="col mb-2">
-                                    <div className="form-check">
-                                        <input
-                                            type="radio"
-                                            className="form-check-input"
-                                            id={`option${index}-${optionIndex}`}
-                                            name={`question${index}`}
-                                            value={option}
-                                            checked={selectedOptions[index] === option}
-                                            onChange={() => handleOptionChange(index, option)}
-                                            disabled={submitted}
-                                        />
-                                        <label
-                                            className={`form-check-label ${
-                                                submitted && option === question.correctOption ? 'text-success' : ''
-                                            } ${
-                                                submitted && option !== question.correctOption ? 'text-danger' : ''
-                                            }`}
-                                            htmlFor={`option${index}-${optionIndex}`}
-                                        >
-                                            {option}
-                                        </label>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        {submitted && (
-                            <div>
-                                <p className={`mt-2 ${selectedOptions[index] === question.correctOption ? 'text-success' : 'text-danger'}`}>
-                                    Your choice: {selectedOptions[index]}
-                                </p>
-                                <p className="mt-2">Explanation: {question.explanation}</p>
-                            </div>
-                        )}
+      <div className="container mt-4">
+        {submitted && result && (
+          <div className="text-center mb-4">
+            <div className="quiz-result">
+              <h4 className="text-center mb-4">Your Quiz Result</h4>
+              <p className="lead">
+                <h1 className="display-6">
+                  Score: {result.correctAnswers} / {result.totalQuestions}
+                </h1>
+              </p>
+              <p>{getResultMessage()}</p>
+            </div>
+          </div>
+        )}
+  
+        <form>
+          {quiz.questions.map((question, index) => (
+            <div key={index} className={`mb-4 border p-3 ${submitted ? 'options' : ''}`}>
+              <p className="lead">{`Question ${index + 1}:`}</p>
+              <Markdown>{question.question}</Markdown>
+              <div className="row row-cols-2">
+                {question.options.map((option, optionIndex) => (
+                  <div key={optionIndex} className="col mb-2">
+                    <div className="form-check">
+                      <input
+                        type="radio"
+                        className="form-check-input"
+                        id={`option${index}-${optionIndex}`}
+                        name={`question${index}`}
+                        value={option}
+                        checked={selectedOptions[index] === option}
+                        onChange={() => handleOptionChange(index, option)}
+                        disabled={submitted}
+                      />
+                      <label
+                        className={`form-check-label ${
+                          submitted && option === question.correctOption ? 'text-success' : ''
+                        } ${
+                          submitted && option !== question.correctOption ? 'text-danger' : ''
+                        }`}
+                        htmlFor={`option${index}-${optionIndex}`}
+                      >
+                        <Markdown>{option}</Markdown>
+                      </label>
                     </div>
+                  </div>
                 ))}
-
-                {!submitted && (
-                    <button type="button" className="btn btn-primary" onClick={handleSubmit}>
-                        Submit Quiz
-                    </button>
-                )}
-            </form>
-        </div>
+              </div>
+              {submitted && (
+                <div>
+                  <p className={`mt-2 ${selectedOptions[index] === question.correctOption ? 'text-success' : 'text-danger'}`}>
+                    Your choice: {selectedOptions[index]}
+                  </p>
+                  <p className="mt-2">
+                    Explanation: <Markdown>{question.explanation}</Markdown>
+                  </p>
+                </div>
+              )}
+            </div>
+          ))}
+  
+          {!submitted && (
+            <button type="button" className="btn btn-primary" onClick={handleSubmit}>
+              Submit Quiz
+            </button>
+          )}
+        </form>
+      </div>
     );
-};
-
+  }
 export default QuizDetail;
